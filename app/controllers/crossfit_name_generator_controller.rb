b312aa0027c44aff7a99ec393f8c05b77385970a
Patrick Jones <pmichaeljones@gmail.com>
2014-08-12 15:03:51 -0600

    finished crossfit name generator

--- app/controllers/crossfit_name_generator_controller.rb
+++ app/controllers/crossfit_name_generator_controller.rb
@@ -4,8 +4,48 @@ class CrossfitNameGeneratorController < ApplicationController
   end
 
   def create
-    binding.pry
-  end
+
+      respond_to do |format|
+
+        format.js do
+
+          #binding.pry
+
+          if params[:query].empty? || ((params[:query] =~ /\s/).nil? == false)
+
+          else
+
+            #binding.pry
+
+            require 'net/http'
+
+            uri = URI("http://words.bighugelabs.com/api/2/#{ENV['BIG_THES_KEY']}" + "/" + "#{params[:query]}" + "/json")
+
+
+            res = Net::HTTP.get_response(uri)
+
+            @reply_code = res.code
+
+            if @reply_code == "404"
+
+            else
+
+              hash = JSON.parse(res.body)
+
+
+              #binding.pry
+
+              @business_names = hash["adjective"]["sim"]
+
+            end
+
+          end #end of if statement
+
+        end #end of format.js
+
+    end #end of respond_to
+
+  end # end of create action
 
   def show
   end
