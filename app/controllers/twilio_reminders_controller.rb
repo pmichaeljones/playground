91c68c307eaaa98489b6e6416829f21c819ea5f7
Patrick Jones <pmichaeljones@gmail.com>
2014-09-05 15:55:45 -0600

    added header to SMS

--- app/controllers/twilio_reminders_controller.rb
+++ app/controllers/twilio_reminders_controller.rb
@@ -34,7 +34,7 @@ require 'twilio-ruby'
     @client = Twilio::REST::Client.new ENV["TWILIO_SID"], ENV["TWILIO_TOKEN"]
 
     message = @client.account.messages.create(
-      :body => "Hi #{reminder.client}. Your #{reminder.event} is ready for pickup",
+      :body => "Patrick's Twilio App: Hi #{reminder.client}. Your #{reminder.event} is ready for pickup",
       :to => "#{user_phone_number}",
       :from => "+17208973141"
       )
