c5e33e462a1532ba9c5cc56b7d0edc23173e5ff2
Patrick Jones <pmichaeljones@gmail.com>
2014-09-05 12:13:43 -0600

    Added reminder buttons

--- app/models/twilio_reminder.rb
+++ app/models/twilio_reminder.rb
@@ -1,2 +1,8 @@
 class TwilioReminder < ActiveRecord::Base
+
+  validates :client, presence: true
+  validates :event, presence: true
+
+  validates :phone_number, presence: true
+  validates :phone_number, length: { is: 12, message: "must be styled like: 555-555-5555" }
 end
