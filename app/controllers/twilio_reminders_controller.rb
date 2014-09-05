1d3674c92ca06c5ec0bc594ba71b3fdf9ea43935
Patrick Jones <pmichaeljones@gmail.com>
2014-09-05 00:08:05 -0600

    formatting of pending events

--- app/controllers/twilio_reminders_controller.rb
+++ app/controllers/twilio_reminders_controller.rb
@@ -1,7 +1,21 @@
 class TwilioRemindersController < ApplicationController
 
   def index
+    @reminders = TwilioReminder.all
     @twilio_reminder = TwilioReminder.new
   end
+
+  def create
+    @twilio_reminder = TwilioReminder.new
+
+
+
+    render :index
+  end
+
+  def send_reminder
+  end
+
+
 
 end
