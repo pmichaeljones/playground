5a88075c5c7c0b68e56380c85e9b886fff2576c6
Patrick Jones <pmichaeljones@gmail.com>
2014-09-05 01:01:52 -0600

    Configured SMS reminder feature

--- app/controllers/twilio_reminders_controller.rb
+++ app/controllers/twilio_reminders_controller.rb
@@ -1,4 +1,5 @@
 class TwilioRemindersController < ApplicationController
+require 'twilio-ruby'
 
   def index
     @reminders = TwilioReminder.all
@@ -14,7 +15,23 @@ class TwilioRemindersController < ApplicationController
   end
 
   def send_reminder
-    binding.pry
+    #binding.pry
+
+    reminder = TwilioReminder.find(params[:id])
+
+
+    @client = Twilio::REST::Client.new ENV["TWILIO_SID"], ENV["TWILIO_TOKEN"]
+
+    message = @client.account.messages.create(
+      :body => "Hi #{reminder.client}. Your #{reminder.event} is ready for pickup",
+      :to => "+16124379264",
+      :from => "+17208973141"
+      )
+
+    message.to
+
+    redirect_to twilio_reminders_path
+
   end
 
 
