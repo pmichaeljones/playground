8850786b6e0aa2cb1cd11daab393eaf7751f6c2c
Patrick Jones <pmichaeljones@gmail.com>
2014-09-08 10:07:57 -0600

    configured ActionMailer for Mandrill

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,16 @@
 Rails.application.configure do
+
+  #ActionMailer settings
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.mandrillapp.com",
+    :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
+    :enable_starttls_auto => true, # detects and uses STARTTLS
+    :user_name => ENV["MANDRILL_USERNAME"],
+    :password  => ENV["MANDRILL_API_KEY"], # SMTP password is any valid API key
+    :authentication => 'login', # Mandrill supports 'plain' or 'login'
+    :domain => 'pmichaeljones.herokuapp.com', # your domain to identify your server when connecting
+  }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
