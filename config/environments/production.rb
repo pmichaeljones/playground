136e0c18f371bb79dd4bcc197170a3d5282deedc
Patrick Jones <pmichaeljones@gmail.com>
2016-06-08 19:15:29 +0200

    added postmark

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,13 +3,12 @@ Rails.application.configure do
 
   #ActionMailer settings
   config.action_mailer.smtp_settings = {
-    :address   => "smtp.mandrillapp.com",
+    :address   => "smtp.postmarkapp.com",
     :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
-    :enable_starttls_auto => true, # detects and uses STARTTLS
-    :user_name => ENV["MANDRILL_USERNAME"],
-    :password  => ENV["MANDRILL_API_KEY"], # SMTP password is any valid API key
-    :authentication => 'login', # Mandrill supports 'plain' or 'login'
-    :domain => 'pmichaeljones.herokuapp.com', # your domain to identify your server when connecting
+    :user_name => ENV["POSTMARK_USERNAME"],
+    :password  => ENV["POSTMARK_PASSWORD"], # SMTP password is any valid API key
+    :authentication => 'plain', # Mandrill supports 'plain' or 'login'
+    :domain => 'patrickjones.me', # your domain to identify your server when connecting
   }
 
   # Code is not reloaded between requests.
